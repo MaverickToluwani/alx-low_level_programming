6b9a6ce4a658cfb5f9950b77f94eafa7383ac37e
MaverickToluwani <micklegend089@gmail.com>
2022-09-14 09:00:01 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabets - Entry point
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+char c = 'a';
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+
+}
