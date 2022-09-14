721b6dd462c01c6966ba0f9e0fbf20f7e28ba92f
MaverickToluwani <micklegend089@gmail.com>
2022-09-14 15:58:44 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -15,11 +16,12 @@ return (1);
 }
 else if (n == 0)
 {
-_putchar('0');
+_putchar(48);
 return (0);
 }
 else if (n < 0)
 {
 _putchar('-');
+}
 return (-1);
 }
