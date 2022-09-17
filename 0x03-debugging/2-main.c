7b95c2a29920d4e4a048284f71435fb04c47858e
MaverickToluwani <micklegend089@gmail.com>
2022-09-16 21:24:50 -0700

    3-print_remaining_days.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
-* main - prints the largest of 3 integers
+* largest_number - prints the largest of 3 integers
 * Return: 0
 */
 
-int main(void)
+int largest_number(int a, int b, int c)
 {
         int a, b, c;
         int largest;
