39586ea9e694ed8f7e4e8d7e262000f3d74c67fb
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-04-18 10:50:21 -0700

    update {comments addition}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,25 +1,30 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of the hash table
+ * Return: Null is memory allocation failed else the hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int j;
-	hash_table_t *table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 	{
-        	perror("Failure creating table");
-        	return NULL;
+		perror("Failure creating table");
+		return (NULL);
 	}
-    	table -> size = size;
-   	table -> array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
-	if (table->array == NULL){
+	table->size = size;
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
 		perror("Memory allocation failed");
-		return NULL;
+		return (NULL);
 	}
-
-   	for (j = 0; j < table->size; j++)
-    	{
-        	table -> array[j] = NULL;
-    	}
-
-    	return (table);
+	for (j = 0; j < table->size; j++)
+	{
+		table->array[j] = NULL;
+	}
+	return (table);
 }
