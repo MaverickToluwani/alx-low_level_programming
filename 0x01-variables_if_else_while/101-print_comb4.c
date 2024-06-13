5810b45ed19e1cdb6b67c09a0dd9ebfed880f338
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-13 05:02:25 -0700

    file additions

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-        int i, j, k;
+	int i, j, k;
 
-        for (i = 48; i < 58; i++)
-        {
-                for (j = 48; j < 58; j++)
-                {
-                        for (k = 48; k < 58; k++)
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
 			{
 				if (j > i && k > j)
-                        	{
-                                	putchar(i);
-                                	putchar(j);
+				{
+					putchar(i);
+					putchar(j);
 					putchar(k);
-                                	if (i != 55)
-                                	{
-                                        	putchar(',');
-                                        	putchar(' ');
-                               		}
-                        	}
+					if (i != 55)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
