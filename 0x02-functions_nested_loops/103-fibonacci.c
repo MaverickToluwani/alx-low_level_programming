84a98198ee4c1254556e0514fa7060cb012e4c5f
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-14 06:54:54 -0700

    file addittion

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * fib_generator - print the first n Fibonacci numbers
+ * @n: number of fibanacci numbers to be printed
+ */
+void fib_generator(void)
+{
+	long int i, start, next, cur, n, sum;
+	sum = 0;
+	start = 1;
+	next = 2;
+	n = 32;
+	for (i = 0; i <= n; i++)
+	{
+		cur = start + next;
+		if (start % 2 == 0)
+			sum = sum + start;
+		start = next;
+		next = cur;
+	}
+	printf("%ld\n", sum);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fib_generator();
+	return (0);
+}
