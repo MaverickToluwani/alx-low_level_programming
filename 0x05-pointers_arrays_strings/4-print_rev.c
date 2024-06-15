70285db2d1ea7e2d8714ea4aade31527341228f6
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-15 15:21:24 -0700

    reverse script fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int i = 0, counter = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-		counter++;
-	}
+	i--;
 
-	while (counter >= 0)
+	while (i >= 0)
 	{
-		_putchar(s[counter]);
-		counter--;
+		_putchar(s[i]);
+		i--;
 	}
-	/*_putchar('\n');*/
-
+	_putchar('\n');
 }
